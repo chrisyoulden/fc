d8a577ebbd9b432c825f006714265cce6393f2be
Chris Youlden <chrisyoulden@yahoo.co.uk>
2020-03-10 11:36:20 +0000

    Initial commit

--- build/__tests__/__fixtures__/RecordingStream.d.ts
+++
@@ -1,6 +0,0 @@
-/// <reference types="node" />
-import { Writable } from 'stream';
-export declare class RecordingStream extends Writable {
-    readonly data: string[];
-    constructor();
-}
--- build/__tests__/__fixtures__/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { RecordingStream } from './RecordingStream';
-export declare const objectRows: {
-    a: string;
-    b: string;
-}[];
-export declare const arrayRows: string[][];
-export declare const multiDimensionalRows: string[][][];
