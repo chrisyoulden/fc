6c1fae4d8032c9744cb3d23ea1ad07ffd8d0cc26
Chris Youlden <chrisyoulden@yahoo.co.uk>
2020-03-10 11:28:15 +0000

    Initial commit

--- build/src/index.d.ts
+++ build/src/index.d.ts
@@ -1,12 +1,14 @@
-/**
- * @projectName fast-csv
- * @github https://github.com/C2FO/fast-csv
- * @includeDoc [Change Log] ../History.md
- * @header [../README.md]
- */
 /// <reference types="node" />
-export { format, write, writeToStream, writeToBuffer, writeToString, writeToPath, FormatterOptionsArgs, Row as FormatterRow, RowMap as FormatterRowMap, RowArray as FormatterRowArray, RowHashArray as FormatterRowHashArray, RowTransformCallback as FormatterRowTransformCallback, RowTransformFunction as FormatterRowTransformFunction, } from './formatter';
-export { parse, parseString, parseStream, parseFile, ParserOptionsArgs, Row as ParserRow, RowMap as ParserRowMap, RowArray as ParserRowArray, RowValidateCallback as ParserRowValidateCallback, SyncRowValidate as ParserSyncRowValidate, AsyncRowValidate as ParserAsyncRowValidate, RowValidate as ParserRowValidate, RowTransformCallback as ParserRowTransformCallback, SyncRowTransform as ParserSyncRowTransform, AsyncRowTransform as ParserAsyncRowTransform, RowTransformFunction as ParserRowTransformFunction, HeaderArray as ParserHeaderArray, HeaderTransformFunction as ParserHeaderTransformFunction, } from './parser';
-export declare const fromString: (string: string, options?: import("./parser").ParserOptionsArgs | undefined) => import("./parser").CsvParserStream;
-export declare const fromStream: (stream: NodeJS.ReadableStream, options?: import("./parser").ParserOptionsArgs | undefined) => import("./parser").CsvParserStream;
-export declare const fromPath: (location: string, options?: import("./parser").ParserOptionsArgs) => import("./parser").CsvParserStream;
+import * as fs from 'fs';
+import { Row } from './types';
+import { FormatterOptionsArgs } from './FormatterOptions';
+import { CsvFormatterStream } from './CsvFormatterStream';
+export * from './types';
+export { CsvFormatterStream } from './CsvFormatterStream';
+export { FormatterOptions, FormatterOptionsArgs } from './FormatterOptions';
+export declare const format: <I extends Row, O extends Row>(options?: FormatterOptionsArgs<I, O> | undefined) => CsvFormatterStream<I, O>;
+export declare const write: <I extends Row, O extends Row>(rows: I[], options?: FormatterOptionsArgs<I, O> | undefined) => CsvFormatterStream<I, O>;
+export declare const writeToStream: <T extends NodeJS.WritableStream, I extends Row, O extends Row>(ws: T, rows: I[], options?: FormatterOptionsArgs<I, O> | undefined) => T;
+export declare const writeToBuffer: <I extends Row, O extends Row>(rows: I[], opts?: FormatterOptionsArgs<I, O>) => Promise<Buffer>;
+export declare const writeToString: <I extends Row, O extends Row>(rows: I[], options?: FormatterOptionsArgs<I, O> | undefined) => Promise<string>;
+export declare const writeToPath: <I extends Row, O extends Row>(path: string, rows: I[], options?: FormatterOptionsArgs<I, O> | undefined) => fs.WriteStream;
--- build/src/index.js
+++ build/src/index.js
@@ -1,26 +1,55 @@
 "use strict";
-/**
- * @projectName fast-csv
- * @github https://github.com/C2FO/fast-csv
- * @includeDoc [Change Log] ../History.md
- * @header [../README.md]
- */
+function __export(m) {
+    for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
+}
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const util_1 = require("util");
-const parser_1 = require("./parser");
-var formatter_1 = require("./formatter");
-exports.format = formatter_1.format;
-exports.write = formatter_1.write;
-exports.writeToStream = formatter_1.writeToStream;
-exports.writeToBuffer = formatter_1.writeToBuffer;
-exports.writeToString = formatter_1.writeToString;
-exports.writeToPath = formatter_1.writeToPath;
-var parser_2 = require("./parser");
-exports.parse = parser_2.parse;
-exports.parseString = parser_2.parseString;
-exports.parseStream = parser_2.parseStream;
-exports.parseFile = parser_2.parseFile;
-exports.fromString = util_1.deprecate(parser_1.parseString, 'csv.fromString has been deprecated in favor of csv.parseString');
-exports.fromStream = util_1.deprecate(parser_1.parseStream, 'csv.fromStream has been deprecated in favor of csv.parseStream');
-exports.fromPath = util_1.deprecate(parser_1.parseFile, 'csv.fromPath has been deprecated in favor of csv.parseFile');
+const stream_1 = require("stream");
+const fs = __importStar(require("fs"));
+const FormatterOptions_1 = require("./FormatterOptions");
+const CsvFormatterStream_1 = require("./CsvFormatterStream");
+__export(require("./types"));
+var CsvFormatterStream_2 = require("./CsvFormatterStream");
+exports.CsvFormatterStream = CsvFormatterStream_2.CsvFormatterStream;
+var FormatterOptions_2 = require("./FormatterOptions");
+exports.FormatterOptions = FormatterOptions_2.FormatterOptions;
+exports.format = (options) => new CsvFormatterStream_1.CsvFormatterStream(new FormatterOptions_1.FormatterOptions(options));
+exports.write = (rows, options) => {
+    const csvStream = exports.format(options);
+    const promiseWrite = util_1.promisify((row, cb) => {
+        csvStream.write(row, undefined, cb);
+    });
+    rows.reduce((prev, row) => prev.then(() => promiseWrite(row)), Promise.resolve())
+        .then(() => csvStream.end())
+        .catch((err) => {
+        csvStream.emit('error', err);
+    });
+    return csvStream;
+};
+exports.writeToStream = (ws, rows, options) => exports.write(rows, options).pipe(ws);
+exports.writeToBuffer = (rows, opts = {}) => {
+    const buffers = [];
+    const ws = new stream_1.Writable({
+        write(data, enc, writeCb) {
+            buffers.push(data);
+            writeCb();
+        },
+    });
+    return new Promise((res, rej) => {
+        ws.on('error', rej).on('finish', () => res(Buffer.concat(buffers)));
+        exports.write(rows, opts).pipe(ws);
+    });
+};
+exports.writeToString = (rows, options) => exports.writeToBuffer(rows, options).then((buffer) => buffer.toString());
+exports.writeToPath = (path, rows, options) => {
+    const stream = fs.createWriteStream(path, { encoding: 'utf8' });
+    return exports.write(rows, options).pipe(stream);
+};
 //# sourceMappingURL=index.js.map
